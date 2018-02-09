0d4d5d9f093d4a2dc0fc343be12a40619fbd3b1a
Karmel Allison <karmel@google.com>
2018-02-09 12:02:39 -0800

    Refining preprocessing, part 3

--- official/resnet/vgg_preprocessing.py
+++ official/resnet/vgg_preprocessing.py
@@ -49,49 +49,6 @@ def _get_h_w(image):
   return shape[0], shape[1]
 
 
-def _crop(image, offset_height, offset_width, crop_height, crop_width):
-  """Crops the given image using the provided offsets and sizes.
-
-  Note that the method doesn't assume we know the input image size but it does
-  assume we know the input image rank.
-
-  Args:
-    image: an image of shape [height, width, channels].
-    offset_height: a scalar tensor indicating the height offset.
-    offset_width: a scalar tensor indicating the width offset.
-    crop_height: the height of the cropped image.
-    crop_width: the width of the cropped image.
-
-  Returns:
-    the cropped (and resized) image.
-
-  Raises:
-    InvalidArgumentError: if the rank is not 3 or if the image dimensions are
-      less than the crop size.
-  """
-  original_shape = tf.shape(image)
-
-  rank_assertion = tf.Assert(
-      tf.equal(tf.rank(image), 3),
-      ['Rank of image must be equal to 3.'])
-  with tf.control_dependencies([rank_assertion]):
-    cropped_shape = tf.stack([crop_height, crop_width, original_shape[2]])
-
-  size_assertion = tf.Assert(
-      tf.logical_and(
-          tf.greater_equal(original_shape[0], crop_height),
-          tf.greater_equal(original_shape[1], crop_width)),
-      ['Crop size greater than the image size.'])
-
-  offsets = tf.to_int32(tf.stack([offset_height, offset_width, 0]))
-
-  # Use tf.slice instead of crop_to_bounding box as it accepts tensors to
-  # define the crop size.
-  with tf.control_dependencies([size_assertion]):
-    image = tf.slice(image, offsets, cropped_shape)
-  return tf.reshape(image, cropped_shape)
-
-
 def _random_crop(image, crop_height, crop_width):
   """Crops the given image.
 
@@ -258,5 +215,5 @@ def preprocess_image(image, output_height, output_width, is_training=False,
   image = _aspect_preserving_resize(image, resize_side)
   image = crop_fn(image, output_height, output_width)
   image.set_shape([output_height, output_width, 3])
-  image = tf.to_float(image)
+  image = tf.cast(image, tf.float32)
   return _mean_image_subtraction(image, [_R_MEAN, _G_MEAN, _B_MEAN])
